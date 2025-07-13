ff83a353d719245da091f794ff2204016e1233f9
MariaLopez35109 <mlopezv1@unicartagena.edu.co>
2025-07-13 01:23:32 -0500

    On this first excercise, we wanted to show the product and sum of two variables, but with one condition, therefore we use conditionals for this excersice, I made some mistakes in the implementation of this activity, since i had to return the variables and also use a function, but the answer was the excpected, the excpected output for both were 600 and 70, and i did it right in that aspect

---
+++ Ej1.py
@@ -0,0 +1,23 @@
+# Calculate the multiplication and sum of two numbers
+# Given two integer numbers, write a Python program to return their product only
+# if the product is equal to or lower than 1000. Otherwise, return their sum.
+
+num1= 20
+num2 = 30
+product = num1*num2
+sum=num1+num2
+if product <= 1000:
+    print("Product =", product)
+else:
+    print("sum =", sum)
+
+
+num3=40
+num4=30
+product = num3*num4
+sum=num3+num4
+
+if product <= 1000:
+    print("Product =", product)
+else:
+    print("sum =", sum)
